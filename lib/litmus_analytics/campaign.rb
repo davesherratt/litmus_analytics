6996f60cb8f27586ce1bcdb34979c955bd207ab6
Dave Sherratt <dave.sherratt@ecrebo.com>
2014-08-14 17:21:22 +0100

    rspec initial

--- lib/litmus_analytics/campaign.rb
+++ lib/litmus_analytics/campaign.rb
@@ -9,6 +9,66 @@ module LitmusAnalytics
       @client.post('/Campaigns', { newCampaign: newCampaign })
     end
 
+    def update_campaign(guid, updateCampaign)
+      @client.put("/Campaigns/#{guid}", { updateCampaign: updateCampaign })
+    end
+
+    def start(guid)
+      @client.put("/Campaigns/#{guid}/Start")
+    end
+
+    def clear(guid)
+      @client.put("/Campaigns/#{guid}/Stop")
+    end
+
+    def email_export_link(guid, export)
+      @client.put("/Campaigns/#{guid}/EmailExportLink", { export: export })
+    end
+
+    def subscribe(guid, subscription)
+      @client.put("/Campaigns/#{guid}/Subscribe", { subscription: subscription })
+    end
+
+    def unsubscribe(guid, subscription)
+      @client.put("/Campaigns/#{guid}/UnSubscribe", { subscription: subscription })
+    end
+
+    def get(campaigns)
+      @client.post('/Campaigns/Get', { campaigns: campaigns })
+    end
+
+    def open_counts(campaigns)
+      @client.post('/Campaigns/OpenCounts', { campaigns: campaigns })
+    end
+
+    def open_counts_exact(year, month, campaigns)
+      @client.post("/Campaigns/OpenCounts/#{year}/#{month}", { campaigns: campaigns })
+    end
+
+    def open_counts_time_frame(fromDate, untilDate, campaigns)
+      @client.post("/Campaigns/OpenCounts/#{fromDate}/#{untilDate}", { campaigns: campaigns })
+    end
+
+    def OpenCountsByMonth(campaigns)
+      @client.post('/Campaigns/OpenCountsByMonth', { campaigns: campaigns })
+    end
+
+    def AdvancedOpenCounts(campaigns)
+      @client.post('/Campaigns/AdvancedOpenCounts', { campaigns: campaigns })
+    end
+
+    def compare(campaigns)
+      @client.post('/Campaigns/Compare', { campaigns: campaigns })
+    end
+
+    def open_counts_for_user(userGuid, fromDate, untilDate)
+      @client.get("/Campaigns/OpenCountsForUser/#{userGuid}/#{fromDate}/#{untilDate}")
+    end
+
+    def open_counts_for_user_month(userGuid, monthDate)
+      @client.get("/Campaigns/OpenCountsForUser/#{userGuid}/#{monthDate}")
+    end
+
     def activity_report(guid)
       @client.get("/Campaigns/#{guid}/ActivityReport")
     end
@@ -33,7 +93,7 @@ module LitmusAnalytics
       @client.get("/Campaigns/#{guid}/MailClientUsageReport")
     end
 
-    def egroup_usage_report(guid)
+    def group_usage_report(guid)
       @client.get("/Campaigns/#{guid}/GroupUsageReport")
     end
 
--- litmus_analytics.gemspec
+++ litmus_analytics.gemspec
@@ -6,8 +6,15 @@ Gem::Specification.new do |s|
   s.description = "Wrapper for the Litmus Analytics API"
   s.summary     = "Wrapper for the Litmus Analytics API"
   s.authors     = ["Dave Sherratt"]
-  s.email       = 'nectom@gmail.com'
-  s.files       = ["lib/litmus_analytics.rb"]
-  s.homepage    = 'http://rubygems.org/gems/litmus_analytics'
+  s.email       = 'nectom@gmail.com' 
+  s.files         = `git ls-files`.split("\n")
+  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
+  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  s.require_paths = ["lib"]
+
+  s.add_development_dependency 'rake'
+  s.add_development_dependency 'rspec'
+
+  s.homepage    = 'http://github.com/davesherratt/litmus_analytics'
   s.license     = 'MIT'
 end
